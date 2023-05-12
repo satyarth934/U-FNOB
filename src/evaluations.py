5c3719fe9baac83ef28c1f81598ef86b62ff1a8c
Satyarth Praveen <satyarth934@gmail.com>
2023-05-12 14:56:26 -0700

    Using the fdl type dataloading at the moment.

--- src/evaluations.py
+++ src/evaluations.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-import training_2 as training    # HACK: Sim-wise dataloader using training_2.py
+import training as training    # HACK: Sim-wise dataloader using training_2.py
 from timeit import default_timer as timer
 
 
