751cb4526ecea9dd9551e52d2b7ed07f57039329
Satyarth Praveen <satyarth934@gmail.com>
2023-05-12 00:42:54 -0700

    Added print statements in between for debugging.

--- src/models/ufno_3D.py
+++ src/models/ufno_3D.py
@@ -2,6 +2,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from models.util_module import PrintShape
+import logging
+logging.basicConfig(filename='model_investigation.log', level=logging.DEBUG)
+
 import operator
 from functools import reduce
 from functools import partial
@@ -70,16 +74,27 @@ class U_net(nn.Module):
 
 
     def forward(self, x):
+        PrintShape(msg="x")(x)
         out_conv1 = self.conv1(x)
+        PrintShape(msg="out_conv1")(out_conv1)
         out_conv2 = self.conv2_1(self.conv2(out_conv1))
+        PrintShape(msg="out_conv2")(out_conv2)
         out_conv3 = self.conv3_1(self.conv3(out_conv2))
+        PrintShape(msg="out_conv3")(out_conv3)
         out_deconv2 = self.deconv2(out_conv3)
+        PrintShape(msg="out_deconv2")(out_deconv2)
         concat2 = torch.cat((out_conv2, out_deconv2), 1)
+        PrintShape(msg="concat2")(concat2)
         out_deconv1 = self.deconv1(concat2)
+        PrintShape(msg="out_deconv1")(out_deconv1)
         concat1 = torch.cat((out_conv1, out_deconv1), 1)
+        PrintShape(msg="concat1")(concat1)
         out_deconv0 = self.deconv0(concat1)
+        PrintShape(msg="out_deconv0")(out_deconv0)
         concat0 = torch.cat((x, out_deconv0), 1)
+        PrintShape(msg="concat0")(concat0)
         out = self.output_layer(concat0)
+        PrintShape(msg="out")(out)
 
         return out
 
@@ -149,28 +164,52 @@ class SimpleBlock3d(nn.Module):
         batchsize = x.shape[0]
         size_x, size_y, size_z = x.shape[1], x.shape[2], x.shape[3]
         
+        # CHECKING MEMORY USAGE
+        max_memory_used = torch.cuda.max_memory_allocated() / 1024**2  # Convert to MB
+        print(f"[SimpleBlock3d - entering forward] Max memory used during forward pass: {max_memory_used:.2f} MB")
+
         x = self.fc0(x)
         x = x.permute(0, 4, 1, 2, 3)
+
+        # CHECKING MEMORY USAGE
+        max_memory_used = torch.cuda.max_memory_allocated() / 1024**2  # Convert to MB
+        print(f"[SimpleBlock3d - after fc0] Max memory used during forward pass: {max_memory_used:.2f} MB")
 
         x1 = self.conv0(x)
         x2 = self.w0(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y, size_z)
         x = x1 + x2 
         x = F.relu(x)
+
+        # CHECKING MEMORY USAGE
+        max_memory_used = torch.cuda.max_memory_allocated() / 1024**2  # Convert to MB
+        print(f"[SimpleBlock3d - after conv0] Max memory used during forward pass: {max_memory_used:.2f} MB")
         
         x1 = self.conv1(x)
         x2 = self.w1(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y, size_z)
         x = x1 + x2 
         x = F.relu(x)
+
+        # CHECKING MEMORY USAGE
+        max_memory_used = torch.cuda.max_memory_allocated() / 1024**2  # Convert to MB
+        print(f"[SimpleBlock3d - after conv1] Max memory used during forward pass: {max_memory_used:.2f} MB")
         
         x1 = self.conv2(x)
         x2 = self.w2(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y, size_z)
         x = x1 + x2 
         x = F.relu(x)
+
+        # CHECKING MEMORY USAGE
+        max_memory_used = torch.cuda.max_memory_allocated() / 1024**2  # Convert to MB
+        print(f"[SimpleBlock3d - after conv2] Max memory used during forward pass: {max_memory_used:.2f} MB")
         
         x1 = self.conv3(x)
         x2 = self.w3(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y, size_z)
         x3 = self.unet3(x) 
         
+        # CHECKING MEMORY USAGE
+        max_memory_used = torch.cuda.max_memory_allocated() / 1024**2  # Convert to MB
+        print(f"[SimpleBlock3d - after conv3] Max memory used during forward pass: {max_memory_used:.2f} MB")
+
         if self.UNet: 
             x = x1 + x2 + x3
         else:
@@ -180,6 +219,10 @@ class SimpleBlock3d(nn.Module):
         x1 = self.conv4(x)
         x2 = self.w4(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y, size_z)
         x3 = self.unet4(x)
+
+        # CHECKING MEMORY USAGE
+        max_memory_used = torch.cuda.max_memory_allocated() / 1024**2  # Convert to MB
+        print(f"[SimpleBlock3d - after conv4] Max memory used during forward pass: {max_memory_used:.2f} MB")
         
         if self.UNet: 
             x = x1 + x2 + x3
@@ -190,6 +233,10 @@ class SimpleBlock3d(nn.Module):
         x1 = self.conv5(x)
         x2 = self.w5(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y, size_z)
         x3 = self.unet5(x)
+
+        # CHECKING MEMORY USAGE
+        max_memory_used = torch.cuda.max_memory_allocated() / 1024**2  # Convert to MB
+        print(f"[SimpleBlock3d - after conv5] Max memory used during forward pass: {max_memory_used:.2f} MB")
         
         if self.UNet: 
             x = x1 + x2 + x3
@@ -201,6 +248,10 @@ class SimpleBlock3d(nn.Module):
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
+
+        # CHECKING MEMORY USAGE
+        max_memory_used = torch.cuda.max_memory_allocated() / 1024**2  # Convert to MB
+        print(f"[SimpleBlock3d - after fc1 and fc2 - exiting forward] Max memory used during forward pass: {max_memory_used:.2f} MB")
         
         return x
 
@@ -216,7 +267,7 @@ class UFNO3d(nn.Module):
 
 
     def forward(self, x):
-        x = x[:, :, :, :64, :]    # considering only first 64 years for numerical sanity. 64 is a power of 2. total 65 yrs available.
+        # x = x[:, :, :, :64, :]    # considering only first 64 years for numerical sanity. 64 is a power of 2. total 65 yrs available. TODO: Move this step to the point where the data is being fed to the model.
         batchsize = x.shape[0]
         # breakpoint()
 
@@ -227,9 +278,10 @@ class UFNO3d(nn.Module):
         #     padding_para = [2,7,8] #z->t, y, x
         padding_para = [0,5,9]
         
+        # breakpoint()
         x = F.pad(F.pad(x, (0,0,0,padding_para[0],0,padding_para[1]), "replicate"), (0,0,0,0,0,0,0,padding_para[2]), 'constant', 0)
-        x = self.conv1(x)
-        x = x.view(batchsize, size_x+padding_para[2], size_y+padding_para[1], size_z+padding_para[0], 4)[..., :-padding_para[2],:-padding_para[1],:-padding_para[0], :]
+        x = self.conv1(x)    # QUESTION: Why is the last dimension=4?
+        x = x.view(batchsize, size_x+padding_para[2], size_y+padding_para[1], size_z+padding_para[0], 4)[..., :-padding_para[2],:-padding_para[1],:(-padding_para[0] if padding_para[0]!=0 else None), :]
         return x.squeeze()
 
 
